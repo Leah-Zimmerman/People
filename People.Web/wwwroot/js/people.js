be6a03f10b93ddb89b66dda6bed9fd2f14921159
Leah Zimmerman <leahzimmermanmonsey@gmail.com>
2024-02-05 13:45:52 -0500

    Message

--- People.Web/wwwroot/js/people.js
+++ People.Web/wwwroot/js/people.js
@@ -1,21 +1,19 @@
 ﻿$(() => {
     let number = 0;
     $("#addrow").on('click', function () {
-        //console.log(GetPreviousValue());
-        //number++;
-        //var number = parseInt(GetPreviousValue()) + 1;
         number++;
-        console.log(number);
         AddRow();
-        console.log(document.getElementById("delete0"));
     })
 
-    $("#delete").on('click', function () {
-        document.getElementById("my-element").remove();
+    $("#rows").on('click', '.btn-danger', function () {
+        console.log("in button");
+        var id = $(this).val();
+        document.getElementById(id).remove();
     })
 
     function AddRow() {
         $("#rows").append(
+            '<div class="row" id="' + number + '">' +
             '<div class="col-md-2 offset-md-2 mt-2">' +
             '<input type="text" name="people[' + number + '].firstname" placeholder="First Name" class="form-control" />' +
             '</div>' +
@@ -23,42 +21,12 @@
             '<input type="text" name="people[' + number + '].lastname" placeholder="Last Name" class="form-control" />' +
             '</div>' +
             '<div class="col-md-2 mt-2">' +
-            '<input type="text" name="people[' + number + '].age" placeholder="Age" class="form-control" />' +
+            '<input "type="text" name="people[' + number + '].age" placeholder="Age" class="form-control" />' +
             '</div>' +
-            '<div id="delete" class="col-md-2 mt-2">' +
-            '<ng form>'+
-            '<p id=' + number + ' class="btn btn-danger">Delete</p>' +
-            '</ng form>'+
-            '</div>' +
+            '<div class="col-md-2 mt-2">' +
+            '<button type="button"  id="delete" value="' + number + '" class="btn btn-danger">Delete</button>' +
+            '</div >' +
             '</div>'
         );
-
     }
-    function GetPreviousValue() {
-        //var v = $("#btn").closest("div#btn").find("button[class='btn btn - primary']").val();
-        //console.log(v);
-
-        //var value = $("#rows").closest("div#age").find("input[placeholder='Age']").val();
-
-        //var value = document.querySelector('input:last-of-type').value;
-
-        //var value = $("#rows").closest("div#age").find("input[placeholder='Age']").val();
-        //console.log($("#rows").closest("div#age").find("input[placeholder='Age']"));
-        //return value;
-    }
-
-    var element = document.getElementById("delete"+number)
-
-    //document.querySelector('body > div:last-of-type').remove();
-
-    //var table = document.getElementById(table1);
-    //var lastRow = table.rows.length;
-
-    //var country = lastRow.getElementById('Country');
-    //country.setAttribute('class', "Country" + lastRow); 
-    //$(".add").on("click", function () {
-    //    var val = $(this).closest("div.options").find("input[name='quantity']").val();
-    //});
-    // the closest ancestor with the id of "div-02"
-    //console.log(el.closest("#div-02")); // <div id="div-02">
 })
